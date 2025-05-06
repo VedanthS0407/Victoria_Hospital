e74f726f6623a1f38d2f2d182c3f65a36032fcb0
Anju1012 <s225136987@deakin.edu.au>
2025-05-06 11:50:55 +1000

    Design Landing Page and server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+
