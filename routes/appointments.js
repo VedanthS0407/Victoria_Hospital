1bc527248d3cb472aaad23b3f1b499a515c89f1b
Anju1012 <s225136987@deakin.edu.au>
2025-05-06 13:05:29 +1000

    Build Booking form

---
+++ routes/appointments.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Appointment = require('../models/Appointment');
+
+// Book new appointment
+router.post('/book', async (req, res) => {
+  const { userId, service, date, time } = req.body;
+  try {
+    await Appointment.create({ userId, service, date, time });
+    res.status(201).send("Appointment booked successfully.");
+  } catch (e) {
+    res.status(400).send("Error booking appointment.");
+  }
+});
+
+// Get appointments for user
+router.get('/user/:userId', async (req, res) => {
+  try {
+    const appts = await Appointment.find({ userId: req.params.userId });
+    res.json(appts);
+  } catch (e) {
+    res.status(500).send("Could not fetch appointments");
+  }
+});
+
+module.exports = router;
