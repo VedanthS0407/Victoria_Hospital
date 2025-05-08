4a244bec06818ea9412961bde77ae72357ab6824
Anju1012 <s225136987@deakin.edu.au>
2025-05-08 18:24:04 +1000

    CSS for Book app

--- models/Appointment.js
+++ models/Appointment.js
@@ -3,12 +3,49 @@ const mongoose = require('mongoose');
 const appointmentSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
+    required: true
   },
-  service: String,
-  date: Date,
-  time: String,
-  status: { type: String, default: 'booked' }
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    min: 1,
+    max: 100,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    match: /^[0-9]{10}$/
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Others'],
+    required: true
+  },
+  department: {
+    type: String,
+    required: true
+  },
+  service: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: 'booked'
+  }
 });
 
 module.exports = mongoose.model('Appointment', appointmentSchema);
